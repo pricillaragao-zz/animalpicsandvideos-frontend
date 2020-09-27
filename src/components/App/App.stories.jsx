75917676d3b343b821db52e858c3e947ebc6ee66
Pricilla Patricia de Aragao <pricillapatricia@gmail.com>
2020-09-27 15:16:44 -0400

    Add storybook

---
+++ src/components/App/App.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import App from './App';
+
+export default {
+  title: 'AnimalPicsAndVideos/App',
+  component: App
+};
+
+const Template = () => <App />
+
+export const Default = Template.bind({});
